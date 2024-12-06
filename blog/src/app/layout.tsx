62bba8dd086c227c2bbe25cf0b9a5d343ded83e7
somedding <tycoontom@naver.com>
2024-12-06 22:25:01 +0900

    테크 블로그 title ㅂ변경

--- blog/src/app/layout.tsx
+++ blog/src/app/layout.tsx
@@ -7,8 +7,8 @@ import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Blog",
-  description: "My personal blog",
+  title: "썸딩의 테크 블로그",
+  description: "썸딩의 테크 블로그입니다.",
   icons: {
     icon: [
       { url: '/favicon.ico' },
@@ -24,8 +24,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" data-theme="light">
-      <body className={`${inter.className} min-h-screen flex flex-col`}>
+    <html lang="ko" data-theme="light">
+      <body className={inter.className}>
         <Navigation />
         <main className="flex-1 bg-base-100">
           {children}
