d950feee0b29e1d50ca52fa103112243625aa360
somedding <tycoontom@naver.com>
2024-12-07 00:57:07 +0900

    d

--- blog/src/app/layout.tsx
+++ blog/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
